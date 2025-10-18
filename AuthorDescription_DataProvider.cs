64c0c237f90c0361b1ca7ce71ac3334a116c9b35
sourav naga <souravnaga97@gmail.com>
2025-10-18 16:57:51 +0530

    Exception Handling, Capture failed test case screenshot, Highlight element added

---
+++ AuthorDescription_DataProvider.cs
@@ -0,0 +1,124 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TesterTalk_Tute.BaseClass;
+using TesterTalk_Tute.Utility;
+using NUnit.Framework;
+using System.Collections;
+
+namespace TesterTalk_Tute
+{
+    public class AuthorDescription_DataProvider
+    {
+        public void HighlightElement(IWebDriver driver, IWebElement element)
+        {
+            IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
+            js.ExecuteScript("arguments[0].style.border='3px solid red'", element);
+        }
+        [Test]
+        [Author("Sourav","souravnaga@gmail.com")]
+        [Description("Sample Description")]
+        public void Auth1()
+        {
+            var Driver = new BrowserUtility().Init("chrome");
+            IWebElement textbox = Driver.FindElement(By.XPath("//input[@id='textInputField']"));
+            textbox.SendKeys("Testing purpose");
+            Thread.Sleep(1000);
+            Driver.Close();
+        }
+        [Test]
+        [Author("Sourav", "souravnaga@gmail.com")]
+        [Description("Sample Description")]
+        public void Auth2()
+        {
+            var Driver = new BrowserUtility().Init("chrome");
+            IWebElement textbox = Driver.FindElement(By.XPath("//input[@id='textInputField']"));
+            textbox.SendKeys("Testing purpose");
+            Thread.Sleep(1000);
+            Driver.Close();
+        }
+        [Test]
+        [Author("Sourav", "souravnaga@gmail.com")]
+        [Description("Sample Description")]
+        [TestCaseSource("getData")]
+        public void Auth3(string browser)
+        {
+            IWebDriver Driver = null;
+            try
+            {
+                Driver = new BrowserUtility().Init(browser);
+                IWebElement textbox = Driver.FindElement(By.XPath("//input[@id='textInputField']"));
+                textbox.SendKeys("Testing purpose");
+                Thread.Sleep(1000);
+                Driver.Quit();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.StackTrace);
+            }
+            finally
+            {
+                if(Driver!=null)
+                {
+                    Driver.Quit();
+                }
+            }
+        }
+        [Test]
+        [Author("Sourav", "souravnaga@gmail.com")]
+        [Description("Sample Description")]
+        public void Auth4()
+        {
+            IWebDriver Driver = null;
+            try
+            {
+                Driver = new BrowserUtility().Init("chrome");
+                IWebElement textbox = Driver.FindElement(By.XPath("//input[@id='textInputField']"));
+                try
+                {
+                    HighlightElement(Driver, textbox);
+                }
+                catch (Exception ex) {
+                    Console.WriteLine("Element not found");
+                    Console.WriteLine(ex.StackTrace);
+                }
+                textbox.SendKeys("Testing purpose");
+                Thread.Sleep(1000);
+                Driver.Quit();
+            }
+            catch (Exception ex)
+            {
+                Screenshot ss = ((ITakesScreenshot)Driver).GetScreenshot();
+                string filePath = Path.Combine("C:\\Users\\soura\\source\\repos\\TesterTalk_Tute\\Screenshots\\", "screenshot.png");
+                ss.SaveAsFile(filePath); // No enum needed
+                Console.WriteLine("Screenshot saved at: " + filePath);
+
+
+                Console.WriteLine(ex.StackTrace);
+                throw;
+            }
+            finally
+            {
+                if (Driver != null)
+                {
+                    Driver.Quit();
+                }
+            }
+        }
+
+        static IList getData()
+        {
+            ArrayList list = new ArrayList();
+            list.Add("chrome");
+            list.Add("firefox");
+            list.Add("edge");
+            return list;
+        }
+        
+    }
+}
--- obj/Debug/net8.0/TesterTalk_Tute.AssemblyInfo.cs
+++ obj/Debug/net8.0/TesterTalk_Tute.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TesterTalk_Tute")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51372dc056e9a40ab200334292890dd74ad7cdc9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75d46ae56d00f3d40d19957edae1eafb60c623f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("TesterTalk_Tute")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TesterTalk_Tute")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
