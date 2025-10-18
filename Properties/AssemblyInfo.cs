51372dc056e9a40ab200334292890dd74ad7cdc9
sourav naga <souravnaga97@gmail.com>
2025-10-18 15:03:39 +0530

    Parallel Test added

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+using NUnit.Compatibility;
+
+[assembly: Parallelizable(ParallelScope.Children)]
+[assembly: LevelOfParallelism(4)]
---
+++ Utility/BrowserUtility.cs
@@ -0,0 +1,21 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesterTalk_Tute.Utility
+{
+    public class BrowserUtility
+    {
+        public IWebDriver Init(IWebDriver driver)
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Url = "https://dd-demo-tau.vercel.app/web_elements.html";
+            return driver;
+        }
+    }
+}
